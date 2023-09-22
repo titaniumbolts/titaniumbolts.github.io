167c57e7cf086274fd7f90c0b6dfe0adf1c0e468
titaniumproducts <60128371+titaniumproducts@users.noreply.github.com>
2023-09-22 12:58:12 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-var smoothJumpUp = function() {
-        if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-            window.scrollBy(0,-50);
-            setTimeout(smoothJumpUp, 10);
+window.onload = function () {
+        var smoothJumpUp = function() {
+                if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+                        window.scrollBy(0,-50);
+                setTimeout(smoothJumpUp, 10);
+                }
         }
-    }
+}
     
-    window.onscroll = function() {
-      var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-      if (scrolled > 100) {
-      	document.getElementById('upbutton').style.display = 'block';
-      } else {
-      	document.getElementById('upbutton').style.display = 'none';
-      }
-    }
+window.onscroll = function() {
+        var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+        if (scrolled > 100) {
+                document.getElementById('upbutton').style.display = 'block';
+        } else {
+                document.getElementById('upbutton').style.display = 'none';
+        }
+}
